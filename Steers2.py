2ba30c52a354bcb6bb73f3441100de963d2258ad
ofostertest <glowing.bab@gmail.com>
2025-02-13 11:25:44 -0600

    Update Steers2.py

--- Steers2.py
+++ Steers2.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,31 +15,36 @@ import gspread
 import pandas as pd
 import time
 
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--no-sandbox") 
+
 CREDENTIALS_PATH = 'credentials.json'
 TOKEN_PATH = 'token.json'
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 def get_google_sheets_service():
-        creds = None
-        if os.path.exists(TOKEN_PATH):
-                creds = Credentials.from_authorized_user_file(TOKEN_PATH, SCOPES)
-        if not creds or not creds.valid:
-                if creds and creds.expired and creds.refresh_token:
-                        creds.refresh(Request())
-                else:
-                        flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_PATH, SCOPES)
-                        creds = flow.run_local_server(port=0, access_type='offline', prompt='consent')
-                with open(TOKEN_PATH, "w") as token:
-                        token.write(creds.to_json())
-        return creds
+	creds = None
+	if os.path.exists(TOKEN_PATH):
+		creds = Credentials.from_authorized_user_file(TOKEN_PATH, SCOPES)
+	if not creds or not creds.valid:
+		if creds and creds.expired and creds.refresh_token:
+			creds.refresh(Request())
+		else:
+			flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_PATH, SCOPES)
+			creds = flow.run_local_server(port=0, access_type='offline', prompt='consent')
+		with open(TOKEN_PATH, "w") as token:
+			token.write(creds.to_json())
+	return creds
 
 def get_sheets_service():
-        creds = authenticate_google_sheets()
-        service = build('sheets', 'v4', credentials=creds)
-        return service
+	creds = authenticate_google_sheets()
+	service = build('sheets', 'v4', credentials=creds)
+	return service
 
-service = Service("/usr/local/bin/chromedriver")
-driver = webdriver.Chrome(service=service)
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
 driver.get("https://public.rma.usda.gov/livestockreports/LRPReport.aspx")
 
