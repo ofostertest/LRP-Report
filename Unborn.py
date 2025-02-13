014b54b4873f45bdff208762e0f30f9e76315226
ofostertest <glowing.bab@gmail.com>
2025-02-13 11:19:53 -0600

    Unborn.py

--- Unborn.py
+++ Unborn.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -12,6 +14,11 @@ import os
 import gspread
 import pandas as pd
 import time
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--no-sandbox") 
 
 CREDENTIALS_PATH = 'credentials.json'
 TOKEN_PATH = 'token.json'
@@ -36,8 +43,8 @@ def get_sheets_service():
 	service = build('sheets', 'v4', credentials=creds)
 	return service
 
-service = Service("/usr/local/bin/chromedriver")
-driver = webdriver.Chrome(service=service)
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
 driver.get("https://public.rma.usda.gov/livestockreports/LRPReport.aspx")
 
