ba4a85b030af0c01beadb554e87683b243189631
ofostertest <glowing.bab@gmail.com>
2025-02-13 12:45:28 -0600

    Update Unborn.py

--- Unborn.py
+++ Unborn.py
@@ -14,6 +14,7 @@ import os
 import gspread
 import pandas as pd
 import time
+import logging
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
@@ -43,6 +44,7 @@ def get_sheets_service():
 	service = build('sheets', 'v4', credentials=creds)
 	return service
 
+logging.basicConfig(level=logging.DEBUG)
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
