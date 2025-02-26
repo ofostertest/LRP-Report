1c1fe347951ae4a6d2dcf4416bc5d18b31ed533d
ofostertest <glowing.bab@gmail.com>
2025-02-26 16:35:24 -0600

    Update Heifers1.py

--- Heifers1.py
+++ Heifers1.py
@@ -114,7 +114,7 @@ try:
         service = build("sheets","v4", credentials=get_google_sheets_service())
                 
         spreadsheet_id = '1eFn_RVcCw3MmdLRGASrYwoCbc1UPfFNVqq1Fbz2mvYg'
-        range_name = 'Sheet1!C379'
+        range_name = 'Sheet1!C39'
         sheet = service.spreadsheets()
         update_values = selected_data
         request = sheet.values().update(spreadsheetId=spreadsheet_id,range=range_name,valueInputOption="RAW",body={"values": update_values}).execute()
