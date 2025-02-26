777a767d9ce8e1294af40b55d6da5db8b4981c57
ofostertest <glowing.bab@gmail.com>
2025-02-26 16:57:25 -0600

    Update Unborn.py

--- Unborn.py
+++ Unborn.py
@@ -132,9 +132,9 @@ try:
 	selected_data = []
 
 	for i, row in enumerate(rows,start=1):
-                if i in selected_rows:
-                        cols = row.find_elements(By.TAG_NAME,"td")   
-                        if len(cols)>13:
+		if i in selected_rows:
+			cols = row.find_elements(By.TAG_NAME,"td")   
+			if len(cols)>13:
 				raw_price_8 = cols[8].text 
 				raw_price_12 = cols[12].text
 				
